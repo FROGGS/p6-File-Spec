ad3ec4d5babe3c4f2fd6b101025932375c67a0f2
Tobias Leich <email@froggs.de>
2012-09-13 23:00:06 +0200

    tmpdir for Win32

--- lib/File/Spec/Win32.pm
+++ lib/File/Spec/Win32.pm
@@ -1,9 +1,26 @@
 
 module File::Spec::Win32;
 
+BEGIN require "File::Spec::Unix";
+
 role File::Spec::OS {
-	method curdir {
-		'.'
+	# use Unix as a base
+	also does File::Spec::Unix;
+
+	# and add Win32 specific stuff
+	my $tmpdir;
+	method tmpdir {
+		return $tmpdir if $tmpdir.defined;
+		$tmpdir = self._tmpdir(
+			%*ENV{'TMPDIR'},
+			%*ENV{'TEMP'},
+			%*ENV{'TMP'},
+			'SYS:/temp',
+			'C:\system\temp',
+			'C:/temp',
+			'/tmp',
+			'/'
+		);
 	}
 }
 
