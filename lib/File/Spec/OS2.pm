1c3502f058d12d819eef8966cac30a7c227f6ba4
Brent Laabs <bslaabs@gmail.com>
2013-04-01 02:25:53 -0700

    initial work on OS/2

--- lib/File/Spec/OS2.pm
+++ lib/File/Spec/OS2.pm
@@ -1,23 +1,27 @@
-class File::Spec::OS2;
+use v6;
+use File::Spec::Unix;
+class File::Spec::OS2 is File::Spec::Unix;
 
 my $module = "File::Spec::Unix";
-require $module;
+#require $module;
 
 method canonpath             { ::($module).canonpath()             }
 method catdir                { ::($module).catdir()                }
 method catfile               { ::($module).catfile()               }
-method curdir                { ::($module).curdir()                }
-method devnull               { ::($module).devnull()               }
-method rootdir               { ::($module).rootdir()               }
 method tmpdir                { ::($module).tmpdir()                }
-method updir                 { ::($module).updir()                 }
 method no_upwards            { ::($module).no_upwards()            }
-method case_tolerant         { 1                                   }
+
+#method curdir                { ::($module).curdir()                }
+#method updir                 { ::($module).updir()                 }
+method devnull               { '/dev/nul'                          }
+#method rootdir               { ::($module).rootdir()               }
+method case_tolerant         { True                                }
+method default_case_tolerant { True                                }
 method file_name_is_absolute { ::($module).file_name_is_absolute() }
 method path                  { ::($module).path()                  }
 method join                  { ::($module).join()                  }
 method splitpath             { ::($module).splitpath()             }
-method splitdir              { ::($module).splitdir()              }
+method splitdir ($dirs)      {  $dirs.split: /<[\\\/]>/            }
 method catpath               { ::($module).catpath()               }
 method abs2rel               { ::($module).abs2rel()               }
 method rel2abs               { ::($module).rel2abs()               }
